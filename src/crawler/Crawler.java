549a35dac310f56a6ddd84d2d62c8a2500fcbc7a
Hactieuho <Hactieuho96@gmail.com>
2016-10-08 21:31:04 +0700

    First java app
    Hien thi mot java applet voi tieu de "First application" ra man hinh.
    21:25
    8/10/2016

---
+++ src/crawler/Crawler.java
@@ -0,0 +1,14 @@
+package crawler;
+import java.awt.*;
+import javax.swing.JFrame;
+public class Crawler extends JFrame{
+	public Crawler(){
+		setTitle("First application");
+		setSize(300, 300);
+	}
+	int a;
+	public static void main(String args[]){
+		Crawler first = new Crawler();
+		first.show();
+	}
+}
